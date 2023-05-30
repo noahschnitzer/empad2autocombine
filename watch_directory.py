644c3f24f120885ff57e26425e035516714fdec5
Noah Schnitzer <nschnitz@umich.edu>
2023-05-30 18:12:26 -0400

    watch for changes in xml and write to console

--- watch_directory.py
+++ watch_directory.py
@@ -1,11 +1,12 @@
 from pathlib import Path
 from watchdog.observers import Observer
-from watchdog.events import LoggingEventHandler
+from watchdog.events import LoggingEventHandler,FileSystemEventHandler
 
 import sys
 import time
 import logging
 
+'''
 if __name__ == "__main__":
     path = sys.argv[1] if len(sys.argv) > 1 else '.'
     path = Path(path)
@@ -22,6 +23,36 @@ if __name__ == "__main__":
     observer = Observer()
     observer.schedule(event_handler, path, recursive=True)
     observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
+'''
+
+
+class MyHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        #print(f'event type: {event.event_type}  path : {event.src_path}')
+        if(event.src_path[-4:] == '.xml'):
+        	with open(event.src_path,'r') as f: # The with keyword automatically closes the file when you are done
+        		print(f.read())
+
+
+
+if __name__ == "__main__":
+    path = sys.argv[1] if len(sys.argv) > 1 else '.'
+    path = Path(path)
+    print(f"{path=}")
+    print(f"{path.exists()=}")
+    print(f"{path.is_dir()=}")
+
+    event_handler = MyHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path=path, recursive=True)
+    observer.start()
+
     try:
         while True:
             time.sleep(1)
